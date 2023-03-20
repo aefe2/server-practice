3ecbc8bbc07291729702ac14d7c82a4c6a556a6e
aefe2 <shigaev_1990@mail.ru>
2023-03-20 05:59:50 +0300

    Создан path.php содержащий настройки путей, обновлен index.php

--- public/index.php
+++ public/index.php
@@ -1,2 +1,13 @@
 <?php
-echo 'hello';
+//Включаем запрет на неявное преобразование типов
+declare(strict_types=1);
+
+try {
+    //Создаем экземпляр приложения и запускаем его
+    $app = require_once __DIR__ . '/../core/bootstrap.php';
+    $app->run();
+} catch (\Throwable $exception) {
+    echo '<pre>';
+    print_r($exception);
+    echo '</pre>';
+}
