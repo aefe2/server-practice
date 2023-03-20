42e3a49ac3236048c2b10f76eac5f4997190ebab
aefe2 <shigaev_1990@mail.ru>
2023-03-20 06:31:08 +0300

    Обновлен метод, конструктор

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,3 @@ use Src\Route;
 
 Route::add('go', [Controller\Site::class, 'index']);
 Route::add('hello', [Controller\Site::class, 'hello']);
-
-require_once __DIR__ . '/../routes/web.php';
-
-return new Src\Application(new Src\Settings(getConfigs()));
