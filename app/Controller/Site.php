61a27ed1556a6fa7592387762dff13a3d9f3722a
Kakayou <shigaev_1990@mail.ru>
2023-03-23 20:48:35 +0700

    Функционал входа выхода

--- app/Controller/Site.php
+++ app/Controller/Site.php
@@ -6,6 +6,7 @@ use Model\Post;
 use Model\User;
 use Src\Request;
 use Src\View;
+use Src\Auth\Auth;
 
 class Site
 {
@@ -15,6 +16,26 @@ class Site
             app()->route->redirect('/go');
         }
         return new View('site.signup');
+    }
+
+    public function login(Request $request): string
+    {
+        //Если просто обращение к странице, то отобразить форму
+        if ($request->method === 'GET') {
+            return new View('site.login');
+        }
+        //Если удалось аутентифицировать пользователя, то редирект
+        if (Auth::attempt($request->all())) {
+            app()->route->redirect('/hello');
+        }
+        //Если аутентификация не удалась, то сообщение об ошибке
+        return new View('site.login', ['message' => 'Неправильные логин или пароль']);
+    }
+
+    public function logout(): void
+    {
+        Auth::logout();
+        app()->route->redirect('/hello');
     }
 
     public function index(Request $request): string
