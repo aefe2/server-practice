5a34d60d15edd00b263912d8298d34770469dc19
Kakayou <shigaev_1990@mail.ru>
2023-03-23 18:22:13 +0700

    Создан класс Post

---
+++ app/Model/Post.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Model;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    use HasFactory;
+
+    public $timestamps = false;
+}
