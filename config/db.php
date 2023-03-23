874a650994ed7948e70da2008d39e7bf771921cf
Kakayou <shigaev_1990@mail.ru>
2023-03-23 18:12:52 +0700

    Добавлены пакеты illuminate/database и illuminate/events, добавлен файл настроек db.php, модифицированы классы

---
+++ config/db.php
@@ -0,0 +1,11 @@
+<?php
+return [
+    'driver' => 'mysql',
+    'host' => 'localhost',
+    'database' => 'mvc',
+    'username' => 'root',
+    'password' => '123',
+    'charset' => 'utf8',
+    'collation' => 'utf8_unicode_ci',
+    'prefix' => '',
+];
--- core/Src/Settings.php
+++ core/Src/Settings.php
@@ -26,6 +26,11 @@ class Settings
         return $this->path['root'] ? '/' . $this->path['root'] : '';
     }
 
+    public function getDbSetting(): array
+    {
+        return $this->db ?? [];
+    }
+
     public function getViewsPath(): string
     {
         return '/' . $this->path['views'] ?? '';
