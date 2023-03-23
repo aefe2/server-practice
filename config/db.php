0e3836567b8946b44baeb848ee57f0b911404df1
Kakayou <shigaev_1990@mail.ru>
2023-03-23 18:24:15 +0700

    Изменен метод index класса Site, убран пароль с БД в связи с ошибкой

--- config/db.php
+++ config/db.php
@@ -4,7 +4,7 @@ return [
     'host' => 'localhost',
     'database' => 'mvc',
     'username' => 'root',
-    'password' => '123',
+    'password' => '',
     'charset' => 'utf8',
     'collation' => 'utf8_unicode_ci',
     'prefix' => '',
