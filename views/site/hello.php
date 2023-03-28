7dc38e2bac4596beb858efba57099bc9e41cefc4
aefe2 <shigaev_1990@mail.ru>
2023-03-28 15:16:48 +0300

    Мем..

--- views/site/hello.php
+++ views/site/hello.php
@@ -1,18 +1,19 @@
 <h2><?= $message ?? ''; ?></h2><br>
-<!--<div style="background-color: black; width: 250px; border-radius: 50%">-->
-<!--    <span class="meme">⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⡀⠀</span><br>-->
-<!--    ⠀⠀⠀<span class="meme">⠉⠉⠉⠙⠻⣿⣿⠿⠿⠛⠛⠛⠻⣿⣿⣇⠀</span><br>-->
-<!--    ⠀⠀<span class="meme">⢤⣀⣀⣀⠀⠀⢸⣷⡄⠀⣁⣀⣤⣴⣿⣿⣿⣆</span><br>-->
-<!--    ⠀⠀⠀⠀<span class="meme">⠹⠏⠀⠀⠀⣿⣧⠀⠹⣿⣿⣿⣿⣿⡿⣿</span><br>-->
-<!--    ⠀⠀⠀⠀⠀⠀⠀⠀⠀<span class="meme">⠛⠿⠇⢀⣼⣿⣿⠛⢯⡿⡟</span><br>-->
-<!--    ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀<span class="meme">⠦⠴⢿⢿⣿⡿⠷⠀⣿⠀</span><br>-->
-<!--    ⠀⠀⠀⠀⠀⠀⠀<span class="meme">⠙⣷⣶⣶⣤⣤⣤⣤⣤⣶⣦⠃⠀</span><br>-->
-<!--    ⠀⠀⠀⠀⠀⠀⠀<span class="meme">⢐⣿⣾⣿⣿⣿⣿⣿⣿⣿⣿⠀⠀</span><br>-->
-<!--    ⠀⠀⠀⠀⠀⠀⠀<span class="meme">⠈⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇⠀⠀</span><br>-->
-<!--    ⠀⠀⠀⠀⠀⠀⠀⠀⠀<span class="meme">⠙⠻⢿⣿⣿⣿⣿⠟</span>-->
-<!--</div>-->
-<!--<style>-->
-<!--    .meme {-->
-<!--        color: white-->
-<!--    }-->
-<!--</style>-->
+<div style="background-color: black; width: 250px; border-radius: 4%">
+    <span class="meme">⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⡀⠀</span><br>
+    ⠀⠀⠀<span class="meme">⠉⠉⠉⠙⠻⣿⣿⠿⠿⠛⠛⠛⠻⣿⣿⣇⠀</span><br>
+    ⠀⠀<span class="meme">⢤⣀⣀⣀⠀⠀⢸⣷⡄⠀⣁⣀⣤⣴⣿⣿⣿⣆</span><br>
+    ⠀⠀⠀⠀<span class="meme">⠹⠏⠀⠀⠀⣿⣧⠀⠹⣿⣿⣿⣿⣿⡿⣿</span><br>
+    ⠀⠀⠀⠀⠀⠀⠀⠀⠀<span class="meme">⠛⠿⠇⢀⣼⣿⣿⠛⢯⡿⡟</span><br>
+    ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀<span class="meme">⠦⠴⢿⢿⣿⡿⠷⠀⣿⠀</span><br>
+    ⠀⠀⠀⠀⠀⠀⠀<span class="meme">⠙⣷⣶⣶⣤⣤⣤⣤⣤⣶⣦⠃⠀</span><br>
+    ⠀⠀⠀⠀⠀⠀⠀<span class="meme">⢐⣿⣾⣿⣿⣿⣿⣿⣿⣿⣿⠀⠀</span><br>
+    ⠀⠀⠀⠀⠀⠀⠀<span class="meme">⠈⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇⠀⠀</span><br>
+    ⠀⠀⠀⠀⠀⠀⠀⠀⠀<span class="meme">⠙⠻⢿⣿⣿⣿⣿⠟</span>
+</div>
+<style>
+    .meme {
+        filter: blur(100%);
+        text-shadow: white 0px 0px 5px;
+    }
+</style>
