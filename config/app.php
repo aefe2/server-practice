9b64bbd80719a38aec2e5153cf0d8f9de263bc48
aefe2 <shigaev_1990@mail.ru>
2023-03-30 13:10:57 +0300

    csrf middleware регистрация

--- config/app.php
+++ config/app.php
@@ -14,7 +14,8 @@ return [
         'unique' => \Validators\UniqueValidator::class
     ],
     'routeAppMiddleware' => [
+        'csrf' => \Middlewares\CSRFMiddleware::class,
         'trim' => \Middlewares\TrimMiddleware::class,
-        'specialChars' => \Middlewares\SpecialCharsMiddleware::class
+        'specialChars' => \Middlewares\SpecialCharsMiddleware::class,
     ]
 ];
